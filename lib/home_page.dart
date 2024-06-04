9d664b31653e74241051dba0c1f39658ec07738a
Yersolid07 <jeremiaturangan@gmail.com>
2024-06-04 09:58:08 +0800

    Sedikit Perubahan

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 // import 'package:flutter/material.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 // class HomePage extends StatelessWidget {
 //   @override
@@ -46,12 +47,32 @@
 //             child: GridView.count(
 //               crossAxisCount: 2,
 //               children: [
+//                 _buildGridTile(context, 'Dashboard',
+//                     FontAwesomeIcons.tachometerAlt, Colors.blue, '/dashboard'),
+//                 _buildGridTile(context, 'Fishing Log',
+//                     FontAwesomeIcons.clipboard, Colors.green, '/fishing_log'),
 //                 _buildGridTile(
-//                     context, 'Maps', Icons.map, Colors.green, '/maps'),
-//                 _buildGridTile(context, 'Jenis Ikan', Icons.pets, Colors.blue,
-//                     '/jenis_ikan'),
-//                 _buildGridTile(context, 'Ulasan', Icons.rate_review,
-//                     Colors.orange, '/ulasan'),
+//                     context,
+//                     'Weather Forecast',
+//                     FontAwesomeIcons.cloudSun,
+//                     Colors.orange,
+//                     '/weather_forecast'),
+//                 _buildGridTile(
+//                     context,
+//                     'Fish Stock Management',
+//                     FontAwesomeIcons.fish,
+//                     Colors.red,
+//                     '/fish_stock_management'),
+//                 _buildGridTile(context, 'Maps', FontAwesomeIcons.map,
+//                     Colors.purple, '/maps_navigation'),
+//                 _buildGridTile(
+//                     context,
+//                     'Community Forum',
+//                     FontAwesomeIcons.comments,
+//                     Colors.brown,
+//                     '/community_forum'),
+//                 _buildGridTile(context, 'Market Prices',
+//                     FontAwesomeIcons.dollarSign, Colors.teal, '/market_prices'),
 //               ],
 //             ),
 //           ),
@@ -81,7 +102,6 @@
 //     );
 //   }
 // }
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -89,6 +109,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xfff9f9f9),
       appBar: AppBar(
         title: Text('Bapancing'),
         actions: [
@@ -100,16 +121,22 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
+      body: ListView(
+        padding: EdgeInsets.all(16.0),
         children: [
           Container(
-            padding: EdgeInsets.all(16.0),
+            margin: EdgeInsets.only(bottom: 16.0),
             child: Row(
               children: [
-                CircleAvatar(
-                  backgroundColor: Colors.blue,
-                  radius: 24.0,
-                  child: Icon(Icons.person, color: Colors.white),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/profil');
+                  },
+                  child: CircleAvatar(
+                    backgroundColor: Colors.blue,
+                    radius: 24.0,
+                    child: Icon(Icons.person, color: Colors.white),
+                  ),
                 ),
                 SizedBox(width: 8.0),
                 Expanded(
@@ -127,38 +154,222 @@ class HomePage extends StatelessWidget {
               ],
             ),
           ),
-          Expanded(
-            child: GridView.count(
-              crossAxisCount: 2,
+          Container(
+            width: 295,
+            height: 51,
+            padding: const EdgeInsets.only(right: 1),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                _buildGridTile(context, 'Dashboard',
-                    FontAwesomeIcons.tachometerAlt, Colors.blue, '/dashboard'),
-                _buildGridTile(context, 'Fishing Log',
-                    FontAwesomeIcons.clipboard, Colors.green, '/fishing_log'),
-                _buildGridTile(
-                    context,
-                    'Weather Forecast',
-                    FontAwesomeIcons.cloudSun,
-                    Colors.orange,
-                    '/weather_forecast'),
-                _buildGridTile(
-                    context,
-                    'Fish Stock Management',
-                    FontAwesomeIcons.fish,
-                    Colors.red,
-                    '/fish_stock_management'),
-                _buildGridTile(context, 'Maps', FontAwesomeIcons.map,
-                    Colors.purple, '/maps_navigation'),
-                _buildGridTile(
-                    context,
-                    'Community Forum',
-                    FontAwesomeIcons.comments,
-                    Colors.brown,
-                    '/community_forum'),
-                _buildGridTile(context, 'Market Prices',
-                    FontAwesomeIcons.dollarSign, Colors.teal, '/market_prices'),
+                Text(
+                  'Selamat Datang, Yermia',
+                  style: TextStyle(
+                    color: Color(0xFF1E2022),
+                    fontSize: 16,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 0.80,
+                  ),
+                ),
+                const SizedBox(height: 7),
+                Text(
+                  'Apa yang pengen dilakuin hari ini?',
+                  style: TextStyle(
+                    color: Color(0xFF77838F),
+                    fontSize: 14,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w400,
+                    letterSpacing: 1,
+                  ),
+                ),
               ],
             ),
+          ),
+          Container(
+            width: 386,
+            height: 109,
+            margin: EdgeInsets.symmetric(vertical: 16.0),
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 0,
+                  top: 0,
+                  child: Container(
+                    width: 386,
+                    height: 109,
+                    decoration: ShapeDecoration(
+                      color: Color(0xFF029EBF),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 14,
+                  top: 19,
+                  child: Container(
+                    width: 91,
+                    height: 71,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          left: 44.49,
+                          top: 18.26,
+                          child: Container(
+                            width: 27.30,
+                            height: 27.39,
+                            decoration: ShapeDecoration(
+                              color: Color(0xFFFEB137),
+                              shape: OvalBorder(),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          left: 33.37,
+                          top: 0,
+                          child: Container(
+                            width: 57.63,
+                            height: 54.77,
+                            child: Stack(),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 119,
+                  top: 14,
+                  child: Container(
+                    width: 90,
+                    height: 81,
+                    padding: const EdgeInsets.only(right: 1),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Manado, Sulawesi Utara',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 1,
+                          ),
+                        ),
+                        const SizedBox(height: 7),
+                        Text(
+                          '28° / 32°',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w400,
+                            letterSpacing: 1,
+                          ),
+                        ),
+                        const SizedBox(height: 7),
+                        Text(
+                          'Selasa, 4 Juni',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w400,
+                            letterSpacing: 1,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 277,
+                  top: 45,
+                  child: Container(
+                    width: 95,
+                    height: 20,
+                    padding: const EdgeInsets.only(right: 1),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 20,
+                          height: 16,
+                          decoration: ShapeDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://via.placeholder.com/20x16"),
+                              fit: BoxFit.fill,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                width: 2,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 14),
+                        Text(
+                          '20km/h',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w400,
+                            letterSpacing: 1,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Text(
+            'Home',
+            style: TextStyle(
+              color: Color(0xFF1E2022),
+              fontSize: 16,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w500,
+              letterSpacing: 0.80,
+            ),
+          ),
+          GridView.count(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            crossAxisCount: 2,
+            crossAxisSpacing: 16.0,
+            mainAxisSpacing: 16.0,
+            children: [
+              _buildGridTile(context, 'Dashboard',
+                  FontAwesomeIcons.tachometerAlt, Colors.blue, '/dashboard'),
+              _buildGridTile(context, 'Fishing Log', FontAwesomeIcons.clipboard,
+                  Colors.green, '/fishing_log'),
+              _buildGridTile(
+                  context,
+                  'Weather Forecast',
+                  FontAwesomeIcons.cloudSun,
+                  Colors.orange,
+                  '/weather_forecast'),
+              _buildGridTile(context, 'Fish Stock Management',
+                  FontAwesomeIcons.fish, Colors.red, '/fish_stock_management'),
+              _buildGridTile(context, 'Maps', FontAwesomeIcons.map,
+                  Colors.purple, '/maps_navigation'),
+              _buildGridTile(context, 'Community Forum',
+                  FontAwesomeIcons.comments, Colors.brown, '/community_forum'),
+              _buildGridTile(context, 'Market Prices',
+                  FontAwesomeIcons.dollarSign, Colors.teal, '/market_prices'),
+            ],
           ),
         ],
       ),
@@ -173,11 +384,13 @@ class HomePage extends StatelessWidget {
       },
       child: Card(
         color: color,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
         child: Center(
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
               Icon(icon, size: 50, color: Colors.white),
+              SizedBox(height: 8.0),
               Text(title, style: TextStyle(color: Colors.white)),
             ],
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,6 @@
 // import 'package:flutter/material.dart';
 // import 'home_page.dart';
-// import 'jenis_ikan_page.dart';
-// import 'produk_page.dart';
-// import 'ulasan_page.dart';
-// import 'maps_page.dart';
-// import 'login_page.dart';
-// import 'profil_page.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Bapancing',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       initialRoute: '/login',
-//       routes: {
-//         '/': (context) => HomePage(),
-//         '/login': (context) => LoginPage(),
-//         '/jenis_ikan': (context) => JenisIkanPage(),
-//         '/produk': (context) => ProdukPage(),
-//         '/ulasan': (context) => UlasanPage(),
-//         '/maps': (context) => MapsPage(),
-//         '/profil': (context) => ProfilPage(),
-//       },
-//     );
-//   }
-// }
-
-import 'package:flutter/material.dart';
-// import 'home_page.dart';
-import 'dashboard_page.dart';
+// import 'dashboard_page.dart';
 // import 'fishing_log_page.dart';
 // import 'weather_forecast_page.dart';
 // import 'fish_stock_management_page.dart';
@@ -67,7 +32,7 @@ import 'dashboard_page.dart';
 //         '/dashboard': (context) => DashboardPage(),
 //         '/fishing_log': (context) => FishingLogPage(),
 //         '/add_fishing_log_entry': (context) => AddFishingLogEntryPage(),
-//         '/weather_forecast': (context) => WeatherForecastPage(),
+//         '/weather_forecast': (context) => WeatherForecastScreen(),
 //         '/fish_stock_management': (context) => FishStockManagementPage(),
 //         '/add_fish_stock': (context) => AddFishStockPage(),
 //         '/maps_navigation': (context) => MapsNavigationPage(),
@@ -80,31 +45,47 @@ import 'dashboard_page.dart';
 //   }
 // }
 
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'dashboard_page.dart';
+import 'fishing_log_page.dart';
+import 'weather_forecast_page.dart';
+import 'fish_stock_management_page.dart';
+import 'maps_navigation_page.dart';
+import 'community_forum_page.dart';
+import 'market_prices_page.dart';
+import 'login_page.dart';
+import 'profil_page.dart';
+import 'add_fish_stock_page.dart';
+import 'add_fishing_log_entry_page.dart';
+import 'add_new_post_page.dart';
+
 void main() {
-  runApp(FisheryApp());
+  runApp(MyApp());
 }
 
-class FisheryApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fishery Management System',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          secondary: Colors.orange,
-        ),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        fontFamily: 'Roboto',
-        textTheme: TextTheme(
-          displayLarge: TextStyle(
-              fontSize: 24, fontWeight: FontWeight.bold, color: Colors.blue),
-          bodyLarge: TextStyle(fontSize: 18, color: Colors.black87),
-          labelLarge:
-              TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-      ),
-      home: DashboardPage(),
+      title: 'Bapancing',
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Poppins'),
+      initialRoute: '/login',
+      routes: {
+        '/': (context) => HomePage(),
+        '/login': (context) => LoginPage(),
+        '/dashboard': (context) => DashboardPage(),
+        '/fishing_log': (context) => FishingLogPage(),
+        '/add_fishing_log_entry': (context) => AddFishingLogEntryPage(),
+        '/weather_forecast': (context) => WeatherForecastScreen(),
+        '/fish_stock_management': (context) => FishStockManagementPage(),
+        '/add_fish_stock': (context) => AddFishStockPage(),
+        '/maps_navigation': (context) => MapsNavigationPage(),
+        '/community_forum': (context) => CommunityForumPage(),
+        '/add_new_post': (context) => AddNewPostPage(),
+        '/market_prices': (context) => MarketPricesPage(),
+        '/profil': (context) => ProfilPage(),
+      },
     );
   }
 }
